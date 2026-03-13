38e4f366f025687ed10294b2200313abfad34da5
Isolation <parodie.clement@gmail.com>
2026-03-14 00:47:40 +0100

    for the king

--- CPP09/ex02/PmergeMe.cpp
+++ CPP09/ex02/PmergeMe.cpp
@@ -2,7 +2,6 @@
 
 PmergeMe::PmergeMe()
 {
-
 }
 
 void PmergeMe::parsing(char **av)
@@ -24,17 +23,17 @@ void PmergeMe::parsing(char **av)
     }
 }
 
-void PmergeMe::sortVector()
+std::vector<int> PmergeMe::fordJohnsonVector(std::vector<int> data)
 {
-    if (vectorData.size() <= 1)
-        return;
+    if (data.size() <= 1)
+        return data;
     std::vector<int> smallNum;
     std::vector<int> bigNum;
 
-    for (size_t i = 0; i + 1 < vectorData.size(); i += 2)
+    for (size_t i = 0; i + 1 < data.size(); i += 2)
     {
-        int x = vectorData[i];
-        int y = vectorData[i + 1];
+        int x = data[i];
+        int y = data[i + 1];
         if (x < y)
         {
             smallNum.push_back(x);
@@ -46,48 +45,88 @@ void PmergeMe::sortVector()
             bigNum.push_back(x);
         }
     }
-    if (vectorData.size() % 2)
-        bigNum.push_back(vectorData.back());
-    std::sort(smallNum.begin(), smallNum.end());
-    for (size_t i = 0; i < bigNum.size(); i++)
+    if (data.size() % 2)
+        bigNum.push_back(data.back());
+    bigNum = fordJohnsonVector(bigNum);
+    std::vector<int> mainData = bigNum;
+    std::vector<int> order = jacobsthalFonction(smallNum);
+    std::vector<bool> inserted(smallNum.size(), false);
+    for (size_t i = 0; i < order.size(); i++)
     {
-        std::vector<int>::iterator it = std::lower_bound(smallNum.begin(), smallNum.end(), bigNum[i]);
-        smallNum.insert(it, bigNum[i]);
+        size_t y = order[i];
+        if (y >= smallNum.size())
+            continue;
+        std::vector<int>::iterator it = std::lower_bound(mainData.begin(), mainData.end(), smallNum[y]);
+        mainData.insert(it, smallNum[y]);
+        inserted[y] = true;
     }
-    vectorData = smallNum;
+    for (size_t i = 0; i < smallNum.size(); i++)
+    {
+        if (!inserted[i])
+        {
+            std::vector<int>::iterator it = std::lower_bound(mainData.begin(), mainData.end(), smallNum[i]);
+            mainData.insert(it, smallNum[i]);
+        }
+    }
+    return mainData;
+}
+
+void PmergeMe::sortVector()
+{
+    vectorData = fordJohnsonVector(vectorData);
+}
+
+std::deque<int> PmergeMe::fordJohnsonDeque(std::deque<int> data)
+{
+    if (data.size() <= 1)
+        return data;
+    std::deque<int> smallNum;
+    std::deque<int> bigNum;
+
+    for (size_t i = 0; i + 1 < data.size(); i += 2)
+    {
+        int x = data[i];
+        int y = data[i + 1];
+        if (x < y)
+        {
+            smallNum.push_back(x);
+            bigNum.push_back(y);
+        }
+        else
+        {
+            smallNum.push_back(y);
+            bigNum.push_back(x);
+        }
+    }
+    if (data.size() % 2)
+        bigNum.push_back(data.back());
+    bigNum = fordJohnsonDeque(bigNum);
+    std::deque<int> mainData = bigNum;
+    std::deque<int> order = jacobsthalFonction(smallNum);
+    std::vector<bool> inserted(smallNum.size(), false);
+    for (size_t i = 0; i < order.size(); i++)
+    {
+        size_t y = order[i];
+        if (y >= smallNum.size())
+            continue;
+        std::deque<int>::iterator it = std::lower_bound(mainData.begin(), mainData.end(), smallNum[y]);
+        mainData.insert(it, smallNum[y]);
+        inserted[y] = true;
+    }
+    for (size_t i = 0; i < smallNum.size(); i++)
+    {
+        if (!inserted[i])
+        {
+            std::deque<int>::iterator it = std::lower_bound(mainData.begin(), mainData.end(), smallNum[i]);
+            mainData.insert(it, smallNum[i]);
+        }
+    }
+    return mainData;
 }
 
 void PmergeMe::sortDeque()
 {
-    if (dequeData.size() <= 1)
-        return;
-    std::deque<int> smallNum;
-    std::deque<int> bigNum;
-
-    for (size_t i = 0; i + 1 < dequeData.size(); i += 2)
-    {
-        int x = dequeData[i];
-        int y = dequeData[i + 1];
-        if (x < y)
-        {
-            smallNum.push_back(x);
-            bigNum.push_back(y);
-        }
-        else
-        {
-            smallNum.push_back(y);
-            bigNum.push_back(x);
-        }
-    }
-    if (dequeData.size() % 2)
-        bigNum.push_back(dequeData.back());
-    std::sort(smallNum.begin(), smallNum.end());
-    for (size_t i = 0; i < bigNum.size(); i++)
-    {
-        std::deque<int>::iterator it = std::lower_bound(smallNum.begin(), smallNum.end(), bigNum[i]);
-        smallNum.insert(it, bigNum[i]);
-    }
-    dequeData = smallNum;
+    dequeData = fordJohnsonDeque(dequeData);
 }
 
 void PmergeMe::printBeforeSort()
--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -24,8 +24,38 @@ class PmergeMe
 
         void parsing(char **av);
         void sortVector();
+        std::vector<int> fordJohnsonVector(std::vector<int> data);
         void sortDeque();
+        std::deque<int> fordJohnsonDeque(std::deque<int> data);
 
         void printBeforeSort();
         void printResult();
+
+        template <typename Container>
+        Container jacobsthalFonction(const Container & c)
+        {
+            Container order;
+            size_t x = c.size();
+            if (x <= 1)
+                return order;
+            std::vector<size_t> jacobsthal;
+            jacobsthal.push_back(1);
+            jacobsthal.push_back(3);
+            while (jacobsthal.back() < x)
+            {
+                size_t size = jacobsthal.size();
+                jacobsthal.push_back(jacobsthal[size - 1] + 2 * jacobsthal[size - 2]); 
+            }
+            size_t prev = 1;
+            for (size_t i = 1; i < jacobsthal.size(); i++)
+            {
+                size_t current = jacobsthal[i];
+                if (current > x)
+                    current = x;
+                for (size_t j = current; j > prev; j--)
+                    order.push_back(j - 1);
+                prev = current;
+            }
+            return order;
+        }
 };
